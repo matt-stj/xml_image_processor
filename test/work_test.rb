c7438afc76bb50695704d4616bb7e639643ac0c7
Matt Stjernholm <thew88@gmail.com>
2016-04-06 23:11:08 -0600

    complete work test

--- test/work_test.rb
+++ test/work_test.rb
@@ -1,8 +1,9 @@
 require_relative '../lib/work'
 require 'minitest/autorun'
 
-class WorkTest
+class WorkTest < MiniTest::Test
   attr_reader :work
+
   def setup
     @work = Work.new("example_thumbnail_url", "example_make", "example_model")
   end
@@ -12,7 +13,11 @@ class WorkTest
   end
 
   def test_it_has_a_make
-    assert_equal work.thumbnail_url, "example_make"
+    assert_equal work.make, "example_make"
+  end
+
+  def test_it_has_a_model
+    assert_equal work.model, "example_model"
   end
 
 end
