feca29009d0e871516b70327e2ce1ef078e7856d
Matt Stjernholm <thew88@gmail.com>
2016-04-07 00:10:18 -0600

    write test for parser

--- test/works_parser_test.rb
+++ test/works_parser_test.rb
@@ -6,11 +6,17 @@ class WorksParserTest < MiniTest::Test
 
   def setup
     @xml_doc = WorksLoader.load_from_api(xml_fixture)
-    binding.pry
   end
 
-  def test_it
+  def test_it_accepts_xml_and_returns_an_array_of_work_objects
+    assert_equal xml_doc.class, Nokogiri::XML::Document
 
+    parsed_xml = WorksParser.parse(xml_doc)
+
+    assert_equal parsed_xml.class, Array
+    parsed_xml.map do |work|
+      assert_equal work.class, Work
+    end
   end
 
   private
