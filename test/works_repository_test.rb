f0cba415ae8a8f88be293edafaaaa9a458698399
Matt Stjernholm <thew88@gmail.com>
2016-04-07 00:58:04 -0600

    complete repo grouping test

--- test/works_repository_test.rb
+++ test/works_repository_test.rb
@@ -2,15 +2,39 @@ require_relative '../lib/works_repository'
 require 'minitest/autorun'
 
 class WorksRepositoryTest < MiniTest::Test
-  attr_reader :works_repository
+  attr_reader :works_repo
 
   def setup
     @works_repo = WorksRepository.new(all_works)
-    binding.pry
   end
 
-  def test_it_works
-    binding.pry
+  def test_it_contains_a_collection_of_works
+    assert works_repo.works
+    works_repo.works.each do |work|
+      assert_equal work.class, Work
+    end
+  end
+
+  def test_it_contains_a_nested_hash_of_makes_models_and_works
+    assert works_repo.makes_and_models
+    assert_equal works_repo.makes_and_models.class, Hash
+
+    i = 0
+    works_repo.makes_and_models.each do |make, model_hash|
+      assert_equal make, "make-#{i}"
+      model_hash.each do |model, work|
+        assert_equal model, "model-#{i}"
+        assert_equal work.class, Array
+        assert_equal work.first.class, Work
+      end
+      i +=1
+    end
+  end
+
+  def test_it_groups_models_of_the_same_make_together
+    repo = WorksRepository.new(duplicate_makes)
+
+    assert_equal repo.makes_and_models["make"].count, 2
   end
 
 
@@ -19,7 +43,15 @@ class WorksRepositoryTest < MiniTest::Test
   def all_works
     works = []
     3.times do |i|
-      works << Work.new("thumbnail-#{i}", "make-#{i}", "model#{i}")
+      works << Work.new("thumbnail-#{i}", "make-#{i}", "model-#{i}")
+    end
+    works
+  end
+
+  def duplicate_makes
+    works = []
+    2.times do |i|
+      works << Work.new("thumbnail-#{i}", "make", "model-#{i}")
     end
     works
   end
